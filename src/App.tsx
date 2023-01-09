1625148bfa58dd9c0ea20ccbd65e94bc0e815929
NXrep <info@nxdec.io>
2023-01-09 15:49:46 +0100

    add mobile warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
+import { lazy, Suspense } from 'react';
 import { Loader } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { VRButton, XR } from '@react-three/xr';
-import { Suspense } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { styles } from 'AppStyles';
-import EmeraldsCatcher from './EmeraldsCatcher';
+import MobileWarning from 'components/MobileWarning';
+const EmeraldsCatcher = lazy(() => import('./EmeraldsCatcher'));
 
 function App() {
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+
   return (
     <>
-      <Suspense fallback={null}>
-        <div style={styles.Instructions}>
-          There are two ways to play the game in Virtual Reality:
-          <br />
-          1️⃣ visit emeralds.nxdec.io in the Meta Quest browser
-          <br />
-          2️⃣ use Chrome or Edge, connect your headset and press Enter VR
-        </div>
-        <VRButton style={styles.VRButton} onClick={() => console.log('clicked me!')} />
-        <Canvas>
-          <XR>
-            <EmeraldsCatcher />
-          </XR>
-        </Canvas>
-      </Suspense>
-      <Loader dataInterpolation={p => `Loading Emeralds Catcher VR ${p.toFixed(2)}% \nPlease wait..`} />
+      {isMobile ? (
+        <MobileWarning />
+      ) : (
+        <>
+          <Suspense fallback={null}>
+            <div style={styles.Instructions}>
+              There are two ways to play the game in Virtual Reality:
+              <br />
+              1️⃣ visit emeralds.nxdec.io in the Meta Quest browser
+              <br />
+              2️⃣ use Chrome or Edge, connect your headset and press Enter VR
+            </div>
+            <VRButton style={styles.VRButton} onClick={() => console.log('clicked me!')} />
+            <Canvas>
+              <XR>
+                <EmeraldsCatcher />
+              </XR>
+            </Canvas>
+          </Suspense>
+          <Loader dataInterpolation={p => `Loading Emeralds Catcher VR ${p.toFixed(2)}% \nPlease wait..`} />
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/MobileWarning.tsx
@@ -0,0 +1,14 @@
+import './MobileWarning.css';
+
+const MobileWarning = (): React.ReactElement => {
+  return (
+    <div className="mobileWarning">
+      <h2>Emeralds Catcher</h2>
+      <p>
+        Please visit emeralds.nxdec.io in the Meta Quest browser or use a web browser on a desktop, connect your headset
+        and press Enter VR.
+      </p>
+    </div>
+  );
+};
+export default MobileWarning;
